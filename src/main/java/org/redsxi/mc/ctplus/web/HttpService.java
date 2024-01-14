eca1ae055cbfd6489eb5cb28f5351d12ed409280
RedsXi <redsxi@qq.com>
2024-01-14 21:16:10 +0800

    remove sound

--- src/main/java/org/redsxi/mc/ctplus/web/HttpService.java
+++ src/main/java/org/redsxi/mc/ctplus/web/HttpService.java
@@ -116,8 +116,7 @@ public class HttpService {
                                 if (itemEntity != null) {
                                     itemEntity.makeFakeItem();
                                 }
-
-                                player.level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ITEM_PICKUP, SoundSource.PLAYERS, 0.2F, ((player.getRandom().nextFloat() - player.getRandom().nextFloat()) * 0.7F + 1.0F) * 2.0F);
+                                //player.level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ITEM_PICKUP, SoundSource.PLAYERS, 0.2F, ((player.getRandom().nextFloat() - player.getRandom().nextFloat()) * 0.7F + 1.0F) * 2.0F);
                                 player.containerMenu.broadcastChanges();
                             } else {
                                 itemEntity = player.drop(itemStack, false);

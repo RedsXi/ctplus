4034859e2b708cdcdd42e809af915df06b2f73ac
RedsXi <redsxi@qq.com>
2024-03-06 01:01:06 +0800

    移除

--- src/main/java/org/redsxi/mc/ctplus/core/Conversion.java
+++
@@ -1,7 +0,0 @@
-package org.redsxi.mc.ctplus.core;
-
-public class Conversion {
-    public static <T, O> T forceConvert(O original, Class<T> clazz) {
-        return (T) original;
-    }
-}
--- src/main/kotlin/org/redsxi/mc/ctplus/util/ArrayUtil.kt
+++
@@ -1,9 +0,0 @@
-package org.redsxi.mc.ctplus.util
-
-object ArrayUtil {
-    fun <T> T.asMutableCollection(): MutableCollection<T> {
-        val list = ArrayList<T>()
-        list.add(this)
-        return list
-    }
-}

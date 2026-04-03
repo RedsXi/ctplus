14e27656e92bcdbc2adb800be400213914447958
RedsXi <redsxi@qq.com>
2026-04-04 02:12:44 +0800

    修改 链路层

--- modules/entry/org/redsxi/transitplus/client/MainClient.kt
+++ modules/entry/org/redsxi/transitplus/client/MainClient.kt
@@ -2,6 +2,26 @@
 
 package org.redsxi.transitplus.client
 
-fun entry() {
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents
+import org.redsxi.mc.ctplus.mapping.Text
+import org.redsxi.transitplus.client.network.NetworkLinkClient
 
+fun entry() {
+    NetworkLinkClient.init()
+    ClientPlayConnectionEvents.JOIN.register { _, _, _ ->
+        NetworkLinkClient.initCurrentConnection()
+    }
+    ClientCommandRegistrationCallback.EVENT.register { dispatcher, _ ->
+        dispatcher.register(ClientCommandManager.literal("ctplus").then(ClientCommandManager.literal("ping").executes {
+            CoroutineScope(Dispatchers.IO).launch {
+                it.source.sendFeedback(Text.literal("Ping success: ${NetworkLinkClient.ping()}MS"))
+            }
+            0
+        }))
+    }
 }

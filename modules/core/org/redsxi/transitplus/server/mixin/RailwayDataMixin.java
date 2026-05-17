1cc2f3db9a15fb9346cc025204a702e4cf768573
RedsXi <redsxi@qq.com>
2026-05-17 13:46:06 +0800

    修改铁路数据访问 添加动态响应数据

--- modules/core/org/redsxi/transitplus/server/mixin/RailwayDataMixin.java
+++ modules/core/org/redsxi/transitplus/server/mixin/RailwayDataMixin.java
@@ -1,13 +1,13 @@
 package org.redsxi.transitplus.server.mixin;
 
-import mtr.data.*;
+import mtr.data.RailwayData;
+import mtr.data.TransportMode;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.redsxi.transitplus.common.data.rail.Rail;
-import org.redsxi.transitplus.common.logger.LoggerKt;
-import org.redsxi.transitplus.server.MixinsKt;
 import org.redsxi.transitplus.server.core.RailwaySystem;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,49 +22,80 @@ import java.util.Map;
 
 @Mixin(value = RailwayData.class, remap = false)
 public class RailwayDataMixin {
-
-    @Unique
-    private static RailwaySystem getRailwaySystem(Level world) {
-        if (world.isClientSide) throw new RuntimeException("Unexcepted client level type");
-        return RailwaySystem.Companion.railwaySystem((ServerLevel) world);
-    }
+    @Shadow
+    @Final
+    private Level world;
 
     @Final
     @Shadow
-    private Level world;
+    private Map<BlockPos, Map<BlockPos, mtr.data.Rail>> rails;
 
-    @Inject(at = @At("HEAD"), method = "addRail(Lnet/minecraft/world/entity/player/Player;Lmtr/data/TransportMode;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;Lmtr/data/Rail;Z)J")
+    @Unique
+    private ServerLevel serverLevel() {
+        if (world.isClientSide) throw new RuntimeException("Current world is not on server side");
+        return (ServerLevel) world;
+    }
+
+    @Unique
+    private RailwaySystem railData() {
+        return RailwaySystem.Companion.railwaySystem(serverLevel());
+    }
+
+    @Inject(
+            at = @At("HEAD"),
+            method = "addRail(Lnet/minecraft/world/entity/player/Player;Lmtr/data/TransportMode;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;Lmtr/data/Rail;Z)J"
+    )
     private void onAppendRail(
             Player player,
             TransportMode transportMode,
             BlockPos posStart,
             BlockPos posEnd,
-            mtr.data.Rail rail,
+            mtr.data.Rail rail0,
             boolean validate,
             CallbackInfoReturnable<Long> cir
     ) {
         if (transportMode != TransportMode.TRAIN) return;
-        getRailwaySystem(world).appendRail(posStart, posEnd, Rail.Companion.read(MixinsKt.accessor(rail)));
+        railData().appendRail(
+                posStart, posEnd,
+                Rail.Companion.read((RailAccessor) rail0)
+        );
     }
 
-    @Inject(at = @At("HEAD"), method = "removeRailConnection(Lnet/minecraft/world/level/Level;Ljava/util/Map;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;)V")
-    private static void onRemoveRail(
-            Level world,
-            Map<BlockPos, Map<BlockPos, mtr.data.Rail>> rails,
-            BlockPos pos1,
-            BlockPos pos2,
+    @Inject(
+            at = @At("HEAD"),
+            method = "removeRailConnection(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/BlockPos;)V"
+    )
+    private void onRemoveRailConnection(
+            Player player,
+            BlockPos posStart,
+            BlockPos posEnd,
             CallbackInfo ci
     ) {
-        getRailwaySystem(world).removeRail(pos1, pos2);
+        railData().removeRail(posStart, posEnd);
     }
 
-    @Inject(at = @At("HEAD"), method = "removeNode(Lnet/minecraft/world/level/Level;Ljava/util/Map;Lnet/minecraft/core/BlockPos;)V")
-    private static void onRemoveNode(
-            Level world,
-            Map<BlockPos, Map<BlockPos, mtr.data.Rail>> rails,
-            BlockPos pos,
+    @Inject(
+            at = @At("HEAD"),
+            method = "removeNode(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/core/BlockPos;Lmtr/data/TransportMode;)V"
+    )
+    private void onRemoveNode(
+            Player player,
+            BlockPos nodePos,
+            TransportMode transportMode,
             CallbackInfo ci
     ) {
-        getRailwaySystem(world).removeNode(pos);
+        if (transportMode != TransportMode.TRAIN) return;
+        railData().removeNode(nodePos);
+    }
+
+    @Inject(
+            at = @At("RETURN"),
+            method = "load"
+    )
+    private void onLoaded(
+            CompoundTag tag,
+            CallbackInfo ci
+    ) {
+        RailwaySystem.Companion.init(serverLevel(), rails);
     }
 }
